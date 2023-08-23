a32ef895ec7986c0ae2284b648f38c7768265ff5
CUJeong <akow283@gmail.com>
2023-08-23 12:49:24 +0900

    update MemoService.java

--- src/chapter20/MemoService.java
+++ src/chapter20/MemoService.java
@@ -3,21 +3,22 @@ package chapter20;
 import java.util.List;
 
 public class MemoService {
+    MemoDAO dao = new MemoDAO();
 
-    public List<MemoVO> selectMemos() {
-        return null;
+    public List<MemoVO> selectMemos() throws Exception {
+        return dao.selectMemos();
     }
-    public MemoVO selectMemo(int selectNo) {
-        return null;
+    public MemoVO selectMemo(int selectNo) throws Exception {
+        return dao.selectMemo(selectNo);
     }
-    public int insertMemo(MemoVO vo) {
-        return 0;
+    public int insertMemo(MemoVO vo) throws Exception {
+        return dao.insertMemo(vo);
     }
-    public int updateMemo(MemoVO vo) {
-        return 0;
+    public int updateMemo(MemoVO vo) throws Exception {
+        return dao.updateMemo(vo);
     }
-    public int deleteMemo(int deleteNo) {
-        return 0;
+    public int deleteMemo(int deleteNo) throws Exception {
+        return dao.deleteMemo(deleteNo);
     }
 
 }
