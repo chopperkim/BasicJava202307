631acee7eff939cddd11251f0f7b2208b06f3039
CUJeong <akow283@gmail.com>
2023-08-23 12:38:17 +0900

    add MemoService.java

---
+++ src/chapter20/MemoService.java
@@ -0,0 +1,23 @@
+package chapter20;
+
+import java.util.List;
+
+public class MemoService {
+
+    public List<MemoVO> selectMemos() {
+        return null;
+    }
+    public MemoVO selectMemo(int selectNo) {
+        return null;
+    }
+    public int insertMemo(MemoVO vo) {
+        return 0;
+    }
+    public int updateMemo(MemoVO vo) {
+        return 0;
+    }
+    public int deleteMemo(int deleteNo) {
+        return 0;
+    }
+
+}
